981b3df4d245b51fa23ec1ecfa13910d38593400
Akshay-cita <edakkatakshay@gmail.com>
2020-05-07 09:02:46 +0530

    Second Push

--- thirdapp/urls.py
+++ thirdapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path,include,re_path
-from thirdapp.views import SchoolListView,SchoolDetailView
+from thirdapp.views import SchoolListView,SchoolDetailView,SchoolCreateView,SchoolUpdateView,SchoolRemoveView
 app_name='basic_app'
 urlpatterns=[
     path('first',SchoolListView.as_view(),name='list'), #CLASS BASED VIEW URL PATTERNS
-    re_path(r'^(?P<pk>[-\w]+)/$',SchoolDetailView.as_view(),name='detail'),
+    re_path(r'^(?P<pk>\d+)/$',SchoolDetailView.as_view(),name='detail'),
+    path('create/',SchoolCreateView.as_view(),name='create'),
+    re_path(r'^update/(?P<pk>\d+)/$',SchoolUpdateView.as_view(),name='update'),
+    re_path(r'^delete/(?P<pk>\d+)/$',SchoolRemoveView.as_view(),name='delete'),
+
  ]
--- thirdapp/views.py
+++ thirdapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import ListView,DetailView,TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,TemplateView,CreateView,UpdateView,DeleteView
 from django.http import HttpResponse
 from thirdapp import models
 # Create your views here.
@@ -21,3 +22,17 @@ class SchoolDetailView(DetailView):
     context_object_name='school_detail'
     model=models.School
     template_name='school_details.html'
+class SchoolCreateView(CreateView):
+    fields=('name','principal','location')
+    model=models.School
+    template_name='school_form.html'
+
+class SchoolUpdateView(UpdateView):
+    fields=('name','principal')
+    model= models.School
+    template_name='school_form.html'
+
+class SchoolRemoveView(DeleteView):
+    model=models.School
+    success_url = reverse_lazy("basic_app:list")
+    template_name='school_confirm_delete.html'
